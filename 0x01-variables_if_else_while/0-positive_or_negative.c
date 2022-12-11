2e99f39de2a1e1024566eb1595a8f30adf5b085d
NBJean90 <fistonngandu@hotmail.com>
2022-12-11 03:05:58 -0800

    task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,11 +1,9 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
-/* more headers goes there */
-
 /**
  * main - Entry point
- *Description - Prints if random generated number is +five, zero or -five
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -14,17 +12,15 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n , 0)
+	if (n < 0)
 	{
 	        printf("%d is negative\n", n);
 	}
-	else if (n == 0)
+	else if (n > 0)
 	{
-	        printf("%d is zero\n", n);
+	        printf("%d is positive\n", n);
 	}
 	else
-	{
-	        printf("%d is positive\n");
-	}
+		printf("%d is zero\n", n);
 	return (0);
 }
