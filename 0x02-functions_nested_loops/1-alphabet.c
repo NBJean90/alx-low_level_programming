abbd3af5284dda5052b1d1f24eb5563babea2363
NBJean90 <fistonngandu@hotmail.com>
2022-12-13 03:30:11 -0800

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet -Entry point
+ *
+ * Return: 0
+ */
+
+void print_alphabet(void)
+
+{
+	char C = 'a';
+
+	while (c <= '2')
+	{
+		putchar(c);
+		c++;
+	}
+
+	_putchar('\n');
+}
